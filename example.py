1cf1c34085047723eac17db14d82cd7765e81979
Filip Klaesson <filipklaesson@Filips-MacBook-Pro.local>
2019-04-14 22:38:37 -0700

    added flow ilp

--- example.py
+++ example.py
@@ -3,23 +3,27 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from itertools import product
-from graph_connectivity.def_ilp import *
+
+#for powerset ilp
+#from graph_connectivity.def_ilp import *
+#for flow ilp
+from graph_connectivity.def_flow_ilp import *
 
 # Define a connectivity graph
 G = Graph()
 
 #Add edges to graph
-connectivity_edges = list(range(4)) #directed connectivity path (one way)
-transition_edges = list(range(4))   #directed transition path (one way)
+connectivity_edges = list(range(20)) #directed connectivity path (one way)
+transition_edges = list(range(20))   #directed transition path (one way)
 G.add_transition_path(transition_edges)
 G.add_connectivity_path(connectivity_edges)
 
 #Set node positions
-node_positions = {i: (0,i) for i in range(4)}   #example {0: (0,0), 1: (1,1)}
+node_positions = {i: (0,i) for i in range(20)}   #example {0: (0,0), 1: (1,1)}
 G.set_node_positions(node_positions)
 
 #Set initial position of agents
-agent_positions = {0: 0, 1: 1, 2: 3}    #agent:position
+agent_positions = {0: 0, 1: 1, 2: 19}    #agent:position
 G.init_agents(agent_positions)
 
 #Plot Graph (saves image as graph.png)
@@ -28,18 +32,22 @@ G.plot_graph()
 # Set up the connectivity problem
 cp = ConnectivityProblem()
 cp.graph = G                            #graph
-cp.T = 2                                #time
-cp.b = {0: 0, 1: 1, 2:3}                #base:node
-cp.static_agents = [0, 2]               #static agents
+cp.T = 9                                #time
+cp.b = {0: 0, 1: 1, 2: 19}              #base:node
+cp.static_agents = []                   #static agents
 
 #Solve (solve() or solve_adaptive())
 st0 = time.time()
-sol = cp.solve_adaptive()
+sol = cp.solve()
 print("solver time: {:.2f}s".format(time.time() - st0))
 
 #Plot Graph (saves image as graph.png)
 cp.plot_solution()
 
+
+
+'''
+#Powerset
 z = cp.solution['x'][0 : cp.num_z]
 e = cp.solution['x'][cp.num_z : cp.num_z + cp.num_e]
 y = cp.solution['x'][cp.num_z + cp.num_e : cp.num_z + cp.num_e + cp.num_y]
@@ -48,3 +56,20 @@ print("Finished")
 print("z:", len(z), '\n', np.reshape(z, (cp.T+1, cp.num_v * cp.num_r)))
 print("e:", len(e), '\n', np.reshape(e, (cp.T, cp.num_v**2)))
 print("y:", len(y), '\n', np.reshape(y, (cp.T+1, cp.num_v * len(cp.b))))
+'''
+
+
+'''
+#Flow
+z = cp.solution['x'][0 : cp.num_z]
+e = cp.solution['x'][cp.num_z : cp.num_z + cp.num_e]
+c = cp.solution['x'][cp.num_z + cp.num_e : cp.num_z + cp.num_e + cp.num_c]
+cbar = cp.solution['x'][cp.num_z + cp.num_e + cp.num_c: cp.num_z + cp.num_e + cp.num_c + cp.num_cbar]
+f = cp.solution['x'][cp.num_z + cp.num_e + cp.num_c +cp.num_cbar : cp.num_z + cp.num_e + cp.num_c +cp.num_cbar + cp.num_f]
+fbar = cp.solution['x'][cp.num_z + cp.num_e + cp.num_c +cp.num_cbar + cp.num_f : cp.num_z + cp.num_e + cp.num_c +cp.num_cbar + cp.num_f + cp.num_fbar]
+
+print("Finished")
+print("z:", len(z), '\n', np.reshape(z, (cp.T+1, cp.num_v * cp.num_r)))
+print("f:", len(f), '\n', np.reshape(f, (cp.T, cp.num_b*cp.num_v**2)))
+print("fbar:", len(fbar), '\n', np.reshape(fbar, (cp.T+1, cp.num_b*cp.num_v**2)))
+'''
